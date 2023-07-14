f0aa0a92ba3c6efd27e6a928b278beca05043620
tsuboi3 <66989243+tsuboi3@users.noreply.github.com>
2023-07-14 17:13:05 +0900

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,13 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
+      <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PK9DWF9');</script>
+<!-- End Google Tag Manager -->
       <Title>Hello World</Title>
       <h1>Hello world!</h1>
       <Counter />
